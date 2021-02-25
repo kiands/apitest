b4e9431685ef5618acbaa7c03053dffef0f3fdea
hanzuofei <07170601@gm.scu.edu.tw>
2021-02-25 14:03:43 +0800

    init

---
+++ app/Todo.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    //取消防止注入的设定（对所有字段都不开启诸如保护）
+    protected $guarded = [];
+}
---
+++ routes/api.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Route::get('/todos',function () {
+//	return response()->json([
+//		['id' => 1,'title' => 'Learn Vue js','completed' => false],
+//		['id' => 2,'title' => 'Go to shop','completed' => false],
+//	]);
+//})->middleware('api');
+
+Route::get('/todos',function () {
+	$todos = App\Todo::all();
+	return $todos;
+})->middleware('api');
+
+Route::post('/todo/create',function (Request $request) {
+	$data = ['title' => $request->get('title'),'completed' => 0];
+	$todo = App\Todo::create($data);
+	return $todo;
+})->middleware('api');
+
+Route::patch('/todo/{id}/completed', function ($id) {
+	$todo = App\Todo::find($id);
+	$todo->completed = ! $todo->completed;
+	$todo->save();
+	return $todo;
+})->middleware('api');
+
+Route::delete('/todo/{id}/delete', function ($id) {
+	$todo = App\Todo::find($id);
+	$todo->delete();
+	return response()->json(['deleted']);
+})->middleware('api');
+
+Route::get('/todo/{id}', function ($id) {
+	$todo = App\Todo::find($id);
+	return $todo;
+})->middleware('api');
+
+Route::get('/counter',function () {
+	$counter = App\Counter::find(1);
+	return $counter->value;
+})->middleware('api');
+
+Route::patch('/counter', function (Request $request) {
+	$counter = App\Counter::find(1);
+	$counter->value = $request->get('value');
+	$counter->save();
+	return $counter;
+})->middleware('api');
